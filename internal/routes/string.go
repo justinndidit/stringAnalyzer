f94869dc4a7ab294f38c29491d540d68b13a350c
Olasoji Favour <favourolasoji@student.oauife.edu.ng>
2025-10-22 05:40:35 +0100

    health check

--- internal/routes/string.go
+++ internal/routes/string.go
@@ -1,6 +1,8 @@
 package routes
 
 import (
+	"net/http"
+
 	chi "github.com/go-chi/chi/v5"
 	"github.com/justinndidit/stringAnalyzer/internal/application"
 )
@@ -12,6 +14,11 @@ func SetupAuthRoutes(app *application.Application) *chi.Mux {
 	r.Get("/strings/{string_value}", app.Handler.GetString)
 	r.Get("/strings/filter-by-natural-language", app.Handler.FilterByNaturalLanguage)
 	r.Delete("/strings/{string_value}", app.Handler.DeleteString)
+	r.Get("/kaithheathcheck", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"status":"ok"}`))
+	})
 
 	return r
 }
